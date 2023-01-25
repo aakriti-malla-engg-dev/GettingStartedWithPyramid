320c63e95f528c87f35ea97dac9692ad78d304eb
aakriti-malla-engg-dev <aakriti.malla@moengage.com>
2023-01-25 19:13:51 +0530

    updates

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from waitress import serve
+from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
-from pyramid.response import Response
+from pyramid.view import view_config
 
 
-def hello_world(request):
-    print('Incoming request')
-    return Response('<body><h1>Hello World!</h1></body>')
+@view_config(
+    route_name='home',
+    renderer='json'
+
+)
+def home(request):
+    return {"a": 1, "b": 2}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with Configurator() as config:
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
+        config.add_route('home', '/')
+        config.scan()
         app = config.make_wsgi_app()
-    serve(app, host='0.0.0.0', port=6543)
- 
+    server = make_server('0.0.0.0', 6543, app)
+    server.serve_forever()
