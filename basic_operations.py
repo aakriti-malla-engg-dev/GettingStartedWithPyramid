1143f182228c5a72aa8bee71b3e2d8e15c60c5a2
aakriti-malla-engg-dev <aakriti.malla@moengage.com>
2023-02-17 18:19:17 +0530

    changes point 3

--- basic_operations.py
+++ basic_operations.py
@@ -9,63 +9,83 @@ collection_name = db["users"]
 
 
 def add_user_to_db(users):
-    collection_name.insert_many(users)
-    print("New User(s) inserted!")
+    inserted = []
+    existing = []
+    for user in users:
+        if collection_name.find_one({"mobile_no": user['mobile_no']}):
+            existing.append(user['mobile_no'])
+        else:
+            collection_name.insert_one(user)
+            inserted.append(user['mobile_no'])
+    result = f"Inserted users: {', '.join(inserted)}\nExisting users: {', '.join(existing)}"
+    return result
 
 
 users = [
-    # {'name': "abc", "mobile_no": "6767676767", "city": "Delhi"},
-    # {'name': "def", "mobile_no": "1234123467", "city": "Delhi"},
+    {'name': "abc", "mobile_no": "6767676767", "city": "Delhi"},
+    {'name': "def", "mobile_no": "1234123467", "city": "Delhi"},
     {'name': "ghi", "mobile_no": "9898989898", "city": "Delhi"}
 ]
 
-
-# add_user_to_db(users)
+# user_added = add_user_to_db(users)
+# print(user_added)
 
 
 def get_user_from_db(mobile_no):
-    print(f"user details for : {mobile_no}")
-    pprint(collection_name.find_one({'mobile_no': mobile_no}))
+    user = collection_name.find_one({'mobile_no': mobile_no})
+    if user:
+        return user
+    else:
+        print('user not found')
 
 
-# get_user_from_db('1234567890')
+# print(get_user_from_db('1234567890'))
 
 
 def get_users_from_db():
-    print("Users are: ")
+    users = []
     for doc in collection_name.find():
-        print(str(doc))
+        users.append(doc)
+    return users
 
 
-# get_users_from_db()
+# print(get_users_from_db())
 
 
 def update_user_in_db(mobile_no, updated_details):
-    collection_name.update_one({"mobile_no": mobile_no}, {"$set": updated_details})
-    print("User detail after update :")
-    pprint(collection_name.find_one({'mobile_no': mobile_no}))
-
-
-# update_user_in_db("2468013579", {'city': 'Bangalore'})
-
-
-def delete_users_from_db(mobile_no):
-    if db.collection_name.find({'mobile_no': {"$in": mobile_no}}):
-        collection_name.delete_one({'mobile_no': mobile_no})
+    user = collection_name.find_one({'mobile_no': mobile_no})
+    if user:
+        collection_name.update_one({"mobile_no": mobile_no}, {"$set": updated_details})
+        return True
     else:
         return False
 
 
-print()
-delete_users_from_db('765766345435')
+# print(update_user_in_db("1234123467", {'city': 'Bangalore'}))
 
-# Return every function with values or True/False
 
-# 1. do not add users with the same mobile no
-# 2. to update the user in a way that the mobile no should be same but the other details should change as mentioned
-# 3. check if number exists for deleting the user otherwise dont make the func (delete) call
+def delete_users_from_db(mobile_no):
+    deleted = collection_name.delete_one({'mobile_no': mobile_no})
+    if deleted.deleted_count == 1:
+        return True
+    else:
+        return False
 
-# 4. build unit test cases
+# delete_user = delete_users_from_db('9898989898')
+# print(delete_user)
+
+
+# -------------TO-DO ---------------
+
+# 1. Add try/catch for internal server error or connection error
+# 2. Return every function with values or True/False
+# 3. Do not add users with the same mobile no (Giving messages if in the list of users added which one was added
+# which one was not) using key/value
+# 4. To update the user in a way that the mobile no should be same but the other details should
+# change as mentioned
+# ✅ 5. check if number exists for deleting the user otherwise dont make the func (delete) call
+
+# 6. build unit test cases
 #     - update unit test -> if number not available through exception user not available
 #     - delete unit test -> Look for number, if not available through exception
 #     - fetch user unit test -> if the user is not available through exception and return something
